cb2476beba46aef193fa79662038afbb00a8759b
Mohit Bishesh <mohitbishesh7@gmail.com>
2023-01-30 17:25:42 +0530

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: ' eu-central-1_Ut2xBZes3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '46nie4nop97at7jfuamvile3uu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
