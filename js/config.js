b8ec120405d74b555025862b417ae98d32254153
Mohit Bishesh <mohitbishesh7@gmail.com>
2023-02-02 14:26:12 +0530

    new_config_correcting

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: ' eu-central-1_Ut2xBZes3', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'eu-central-1_Ut2xBZes3', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '46nie4nop97at7jfuamvile3uu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
